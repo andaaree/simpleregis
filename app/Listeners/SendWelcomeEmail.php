886ecc26451107a88ab7a90a3367baf36f499cce
Andar <50786504+andaaree@users.noreply.github.com>
2025-12-08 01:14:40 +0700

    feat(email): add welcome email listener

---
+++ app/Listeners/SendWelcomeEmail.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Mail\WelcomeEmail;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Mail;
+
+class SendWelcomeEmail implements ShouldQueue
+{
+    use InteractsWithQueue;
+
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(Registered $event): void
+    {
+        Mail::to($event->user->email)->send(new WelcomeEmail($event->user));
+    }
+}
